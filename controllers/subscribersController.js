3f0e98b082adc9b06ec0f3d825b15aa8ca6ba9a6
Kianni <nikolaeff.k.a@gmail.com>
2020-01-22 14:35:40 +0200

    CRUD toimii subscribersilla

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -28,6 +28,11 @@ module.exports = {
   },
   create: (req, res, next) => {
     let subscriberParams = getSubscriberParams(req.body);
+              /*{
+              name: req.body.name,
+              email: req.body.email,
+              zipCode: req.body.zipCode
+            };*/
 
     Subscriber.create(subscriberParams)
       .then(subscriber => {
--- main.js
+++ main.js
@@ -10,30 +10,47 @@ const express = require("express"),
   router = express.Router(),
   methodOverride = require("method-override");
 
-  mongoose.connect(
-    "mongodb://localhost:27017/confetti_cuisine",
-    {useNewUrlParser: true});
   mongoose.Promise = global.Promise;
 
-app.use("/", router);
-app.use(express.static("public"));
-app.use(
-  express.urlencoded({
-    extended: false
-  })
-);
-app.use(express.json());
-app.use(methodOverride("_method", {methods: ["POST","GET"]}));
-
-
-
-  app.set("port", process.env.PORT || 3000);
   app.set("view engine", "ejs");
   app.use(layouts);
 
+  app.use(
+    express.urlencoded({
+      extended: false
+    })
+  );
+  app.use(express.json());
+
+  app.set("port", process.env.PORT || 3000);
+  app.listen(app.get("port"), () => {
+    console.log(
+      `Server running at http://localhost:${app.get("port")}`
+    );
+  });
+
+  mongoose.connect(
+    "mongodb://localhost:27017/confetti_cuisine",
+    {useNewUrlParser: true});
+
+  const db = mongoose.connection;
+
+  db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!")
+  })
+
+
+app.use("/", router);
+app.use(express.static("public"));
+
+router.use(methodOverride("_method", {
+  methods: ["POST","GET"]
+}));
+
+
 router.get("/subscribers", subscribersController.index, subscribersController.indexView);
 router.get("/subscribers/new", subscribersController.new);
-router.post("/subscribers/create", subscribersController.create);
+router.post("/subscribers/create", subscribersController.create, subscribersController.redirectView);
 router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
 router.get("/subscribers/:id/edit", subscribersController.edit);
 router.put("/subscribers/:id/update", subscribersController.update, subscribersController.redirectView);
@@ -49,9 +66,3 @@ router.delete("/subscribers/:id/delete", subscribersController.delete, subscribe
 //why use?
   //router.use(errorController.pageNotFound);
   //router.use(errorController.internalServerError);
-
-  app.listen(app.get("port"), () => {
-    console.log(
-      `Server running at http://localhost:${app.get("port")}`
-    );
-  });
